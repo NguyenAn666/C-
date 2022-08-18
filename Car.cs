0d2e99c4f6aba5ed77f7fd78831e4c9abe678b96
NguyenAn666 <97713637+NguyenAn666@users.noreply.github.com>
2022-08-18 23:21:30 +0700

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿namespace C_sharp
+{
+    internal class Car
+    {
+        public string make;
+        public string model;
+        public string color;
+        public int yearBuilt;
+        public void Start()
+        {
+            System.Console.WriteLine(model + " started");
+        }
+        public void Stop()
+        {
+            System.Console.WriteLine(model + " stopped");
+        }
+    }
+}
